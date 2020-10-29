9cb6603e749928abac587f384c6867882dda3b06
GurneckJohal <gurneck.johal@ucalgary.ca>
2020-10-29 00:09:39 -0600

    Basic GPS toggle button added
    Added a basic GPS toggle without GPS functionality added

--- app/src/main/java/com/cpsc571/footprints/TrackerActivity.kt
+++ app/src/main/java/com/cpsc571/footprints/TrackerActivity.kt
@@ -2,10 +2,28 @@ package com.cpsc571.footprints
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.ToggleButton
+import kotlinx.android.synthetic.main.activity_tracker.*
 
 class TrackerActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tracker)
+        gpsButtonListenerEnable()
+    }
+
+    fun gpsButtonListenerEnable(){
+        val toggle: ToggleButton = findViewById(R.id.toggleButton2)
+        toggle.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // The toggle is enabled
+                val textView = testingTextView.apply{text = "GPS ON"}
+            } else {
+                // The toggle is disabled
+                val textView = findViewById<TextView>(R.id.testingTextView).apply{text = "GPS OFF"}
+            }
+        }
     }
 }
