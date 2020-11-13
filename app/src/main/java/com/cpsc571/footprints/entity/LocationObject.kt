99fd97712c83e2537e158648a4156c8e86ee32bf
GurneckJohal <gurneck.johal@ucalgary.ca>
2020-11-12 19:56:29 -0700

    Tracker now sends location to database
    Sends location to database when the user names a found location, also fixed big where clicking outside of popup dialog freezes location finding

---
+++ app/src/main/java/com/cpsc571/footprints/entity/LocationObject.kt
@@ -0,0 +1,10 @@
+package com.cpsc571.footprints.entity
+
+class LocationObject(
+        var address: String?,
+        var longitude: String?,
+        var latitude: String?
+): JsonObject()
+{
+
+}
