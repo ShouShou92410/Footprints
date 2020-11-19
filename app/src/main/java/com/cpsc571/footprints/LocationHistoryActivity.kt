93ba6e7eb1fb0866e6f1d13ab8abd1f83bdd0ed4
Anthony Tang <anthony.tang2@ucalgary.ca>
2020-11-19 04:07:54 -0700

    Shows locations of user
    LocationHistoryActivity: Fetches locations, and creates ui elements
    dynamically
    LocationObject: Overloaded constructor to create location object from a
    DataSnapshot from Firebase
    activity_location_history.xml: Removed old ui, added new RecyclerView
    element
    
    Added new files:
    border.xml: A simple border for view layouts
    location_history_locations_iterable: LocationHistoryActivity's location
    ui objects

--- app/src/main/java/com/cpsc571/footprints/LocationHistoryActivity.kt
+++ app/src/main/java/com/cpsc571/footprints/LocationHistoryActivity.kt
@@ -3,6 +3,13 @@ package com.cpsc571.footprints
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cpsc571.footprints.entity.LocationObject
 import com.cpsc571.footprints.firebase.FirebaseFootprints
 import com.cpsc571.footprints.firebase.FirebaseFootprintsSource
 import com.google.firebase.auth.ktx.auth
@@ -14,25 +21,71 @@ class LocationHistoryActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_location_history)
 
-        getLocations()
+        val adapter = setupLocationsList()
+        getAndDisplayLocations(adapter)
     }
 
-    private fun getLocations() {
-        var firebaseFootprints: FirebaseFootprints = FirebaseFootprintsSource()
-        var currentUser = Firebase.auth.currentUser
-        var onChange: (DataSnapshot) -> Unit = {
+    private fun getAndDisplayLocations(adapter: RecyclerView.Adapter<CustomAdapter.ViewHolder>) {
+        val firebaseFootprints: FirebaseFootprints = FirebaseFootprintsSource()
+        val currentUser = Firebase.auth.currentUser
+        val onChange: (DataSnapshot) -> Unit = {
             value: DataSnapshot ->
-                Log.d("a", "a")
-                var locations = value.children.find { childSnapshot: DataSnapshot -> childSnapshot.key == "locations" }
-                if (locations != null && locations.exists()) {
+                val locations = value.children.find { childSnapshot: DataSnapshot -> childSnapshot.key == "locations" }
+                if (locations != null && !locations.exists()) {
                     // TODO Show empty locations
                 } else {
+                    val data = mutableListOf<LocationObject>()
                     locations?.children?.forEach(fun(locationSnapshot: DataSnapshot) {
-                        Log.d("t", "t")
+                        data.add(LocationObject(locationSnapshot))
                     })
+                    val adapter = CustomAdapter(data.toTypedArray())
+                    setupLocationsList(adapter)
                 }
         }
         firebaseFootprints.get("Users/${currentUser?.uid.toString()}", onChange)
-
     }
+
+    private fun setupLocationsList(adapter: CustomAdapter = CustomAdapter(arrayOf())): RecyclerView.Adapter<CustomAdapter.ViewHolder> {
+        val locationList = findViewById<RecyclerView>(R.id.locationList)
+        locationList.adapter = adapter
+        locationList.layoutManager = LinearLayoutManager(this)
+        return adapter
+    }
+}
+
+private class CustomAdapter(private val dataSet: Array<LocationObject>) :
+        RecyclerView.Adapter<CustomAdapter.ViewHolder>() {
+
+    /*
+    This is where you define what the list elements are
+     */
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        val locationName: TextView = view.findViewById(R.id.locationName)
+        val locationAddress: TextView = view.findViewById(R.id.locationAddress)
+
+        init {
+            // Define click listener for the ViewHolder's View.
+        }
+    }
+
+    // Create new views (invoked by the layout manager)
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): ViewHolder {
+        // Create a new view, which defines the UI of the list item
+        val view = LayoutInflater.from(viewGroup.context)
+                .inflate(R.layout.location_history_locations_iterable, viewGroup, false)
+
+        return ViewHolder(view)
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    override fun onBindViewHolder(viewHolder: ViewHolder, position: Int) {
+
+        // Get element from your dataset at this position and replace the
+        // contents of the view with that element
+        viewHolder.locationName.text = dataSet[position].name
+        viewHolder.locationAddress.text = dataSet[position].address
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    override fun getItemCount() = dataSet.size
 }
--- app/src/main/java/com/cpsc571/footprints/entity/LocationObject.kt
+++ app/src/main/java/com/cpsc571/footprints/entity/LocationObject.kt
@@ -1,5 +1,8 @@
 package com.cpsc571.footprints.entity
 
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+
 class LocationObject(
         var name: String?,
         var address: String?,
@@ -7,4 +10,9 @@ class LocationObject(
         var latitude: String?
 ): JsonObject()
 {
+    constructor(firebaseObject: DataSnapshot):
+            this(firebaseObject.child("name").value.toString(),
+                firebaseObject.child("address").value.toString(),
+                firebaseObject.child("longitude").value.toString(),
+                firebaseObject.child("latitude").value.toString())
 }
