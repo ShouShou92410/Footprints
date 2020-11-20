e0dca797cf5e800c3bff7def1bc165df9e175fa3
GurneckJohal <gurneck.johal@ucalgary.ca>
2020-11-19 18:03:13 -0700

    Potential Out of screen ui fix
    checkpoint + potential out of screen ui fix

--- app/src/main/java/com/cpsc571/footprints/LocationSelectedActivity.kt
+++ app/src/main/java/com/cpsc571/footprints/LocationSelectedActivity.kt
@@ -1,9 +1,19 @@
 package com.cpsc571.footprints
 
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.cpsc571.footprints.entity.LocationObject
+import com.cpsc571.footprints.entity.PurchaseObject
+import com.cpsc571.footprints.firebase.FirebaseFootprints
+import com.cpsc571.footprints.firebase.FirebaseFootprintsSource
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.database.DataSnapshot
+import com.google.firebase.ktx.Firebase
 
 class LocationSelectedActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,6 +39,34 @@ class LocationSelectedActivity : AppCompatActivity() {
         }
 
         //Log.d("LocationSelectedActivty", "longitude: ${longitude}, latitude: $latitude")
-
     }
+    /*
+    private fun getAndDisplayPurchases(adapter: RecyclerView.Adapter<LocationSelectedActivity.CustomAdapter.ViewHolder>) {
+        val firebaseFootprints: FirebaseFootprints = FirebaseFootprintsSource()
+        val currentUser = Firebase.auth.currentUser
+        val onChange: (DataSnapshot) -> Unit = {
+            value: DataSnapshot ->
+            val purchases = value
+            if (purchases != null && !purchases.exists()) {
+                // TODO Show empty locations
+            } else {
+                val data = mutableListOf<PurchaseObject>()
+                purchases?.children?.forEach(fun(purchaseSnapshot: DataSnapshot) {
+                    data.add(PurchaseObject(purchaseSnapshot))
+                })
+                val adapter = LocationSelectedActivity.CustomAdapter(data.toTypedArray())
+                setupPurchasesList(adapter)
+            }
+        }
+        firebaseFootprints.get("Purchases/${currentUser?.uid}/", onChange)
+    }
+
+    private fun setupPurchasesList(adapter: LocationSelectedActivity.CustomAdapter = LocationSelectedActivity.CustomAdapter(arrayOf())): RecyclerView.Adapter<LocationSelectedActivity.CustomAdapter.ViewHolder> {
+        val purchaseList = findViewById<RecyclerView>(R.id.spendingList)
+        purchaseList.adapter = adapter
+        purchaseList.layoutManager = LinearLayoutManager(this)
+        return adapter
+    }
+*/
+
 }
---
+++ app/src/main/java/com/cpsc571/footprints/entity/PurchaseObject.kt
@@ -0,0 +1,14 @@
+package com.cpsc571.footprints.entity
+
+import android.util.Log
+import com.google.firebase.database.DataSnapshot
+
+class PurchaseObject(
+        var name: String?,
+        var cost: String?
+): JsonObject()
+{
+    constructor(firebaseObject: DataSnapshot):
+            this(firebaseObject.child("name").value.toString(),
+                    firebaseObject.child("cost").value.toString())
+}
