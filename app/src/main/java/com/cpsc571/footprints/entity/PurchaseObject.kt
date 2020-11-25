ed92e1a0ca0778b5237b84b7d51004fb8431398b
GurneckJohal <gurneck.johal@ucalgary.ca>
2020-11-24 19:03:41 -0700

    added date to purchase object
    description

--- app/src/main/java/com/cpsc571/footprints/entity/PurchaseObject.kt
+++ app/src/main/java/com/cpsc571/footprints/entity/PurchaseObject.kt
@@ -1,16 +1,19 @@
 package com.cpsc571.footprints.entity
 
-import android.util.Log
+import android.os.Build
+import androidx.annotation.RequiresApi
 import com.google.firebase.database.DataSnapshot
+import java.time.LocalDate
 
 class PurchaseObject(
         var total: String?,
         var purchaseDetailKey: String?,
-        var date: String?
+        var date: LocalDate
 ): JsonObject()
 {
+    @RequiresApi(Build.VERSION_CODES.O)
     constructor(firebaseObject: DataSnapshot):
             this(firebaseObject.child("total").value.toString(),
                     firebaseObject.child("purchaseDetailKey").value.toString(),
-                        firebaseObject.child("date").value.toString())
+                        LocalDate.parse(firebaseObject.child("date").value.toString() ))
 }
